8b70998eaf382aae25ec8b7e8d08416a4c613690
elliehay <elliehayward44@yahoo.com>
2017-05-05 11:32:06 +0100

    h3 headings fade in

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
 function main() {
 $('h1').hide();
 $('.extrahiding').hide();
+$('h3').hide();
 $('h1').fadeIn(500);
 $('.extrahiding').fadeIn(1000);
-
+$('h3').fadeIn(1000);
 }
 $(document).ready(main);
